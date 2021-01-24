c843455a895d8c6fa286f799ce90a1ed0c0644dc
Thamil <thamilpro@gmail.com>
2021-01-24 16:10:17 +0530

    Bit code change

--- src/index.js
+++ src/index.js
@@ -2,49 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const firstBook={
-title:"Such a Fun Age",
-author:"Kiley Reid",
-image:"https://images-eu.ssl-images-amazon.com/images/I/51TEBoy4lnL._AC_SX184_.jpg"
-};
+const books = [
+  {
+    id: 1,
+    title: "Such a Fun Age",
+    author: "Kiley Reid",
+    image:
+      "https://images-eu.ssl-images-amazon.com/images/I/51TEBoy4lnL._AC_SX184_.jpg",
+  },
+  {
+    id: 2,
+    title: "Home Body",
+    author: "Rupi KaurRupi Kaur",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/41XWaq6RhLL._AC_SX184_.jpg",
+  },
+];
 
-const secondBook = {
-  title: "Home Body",
-  author: "Rupi KaurRupi Kaur",
-  image:
-    "https://images-na.ssl-images-amazon.com/images/I/41XWaq6RhLL._AC_SX184_.jpg",
-};
+
+
+
 function BookList(){
   return (
     <section className="bookList">
-      <Books
-        image={firstBook.image}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Books
-        image={secondBook.image}
-        title={secondBook.title}
-        author={secondBook.author}
-      >
-        <p>
-          Joyce Meyer is one of the world's leading practical Bible teachers.
-          Her daily broadcast, Enjoying Everyday Life, airs on hundreds of
-          television networks and radio stations worldwide
-        </p>
-      </Books>
+      {books.map((book) => {
+        const { title, author, image } = book;
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
-const Books = (props) => {
-  const { image, title, author ,children} = props;
+
+const Book = ({title, author, image}) => {
+  // const { title, author, image } = props;
   return (
     <article className="book">
-      <img src={image} alt=""></img>
+      <img src={image} alt="" />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      <h4>{children}</h4>
     </article>
   );
 };
